40627d4a9e29f90215fbccbc35bc1f97f2ad9591
Maggie Greene <maggie.greene96@gmail.com>
2019-12-28 22:20:02 -0600

    Players print to team container

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import playerShape from '../../helpers/propz/playerShape';
 
+import './Player.scss';
+
 class Player extends React.Component {
   static propTypes = {
     player: playerShape.playerShape,
@@ -11,14 +13,14 @@ class Player extends React.Component {
     const { player } = this.props;
 
     return (
-      <div className="Player col-4">
+      <div className="Player col-3">
         <div className="card">
         <img src={player.imageUrl} className="card-img-top boardImg" alt={player.name} />
           <div className="card-body">
             <h5 className="card-title">{player.name}</h5>
-            <p className="card-text">{player.description}</p>
+            <p className="card-text">{player.position}</p>
             <button className="btn btn-light">View Player</button>
-            <button className="btn btn-secondary">Edit Player</button>
+            <button className="btn btn-light">Edit Player</button>
           </div>
         </div>
       </div>
